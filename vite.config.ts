a4d50b0e4fb0a87680aa405883cf769f736982a4
iwmy10 <iwau.dev@gmail.com>
2024-05-21 09:26:57 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import { markdownToHtml } from "./md-to-html";
 import { resolve } from "path";
 import { globSync } from "glob";
 
+console.log("---process.env.GITHUB_PAGES--------------");
+console.log(JSON.stringify(process.env.BASE_PATH));
+console.log("---process.env.GITHUB_PAGES--------------");
+
 export default defineConfig({
-  base: "/<REPO>/",
+  base: process.env.BASE_PATH || "/",
   root: resolve(__dirname, "src"),
   resolve: {},
   build: {
