53c2e34cc5b686707ea9b2142301d748ca01623d
iwmy10 <iwau.dev@gmail.com>
2024-05-25 16:51:16 +0900

    html内のaタグの絶対パスを、vite.configのbaseの値で置き換える

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { readFileSync } from "fs";
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import { markdownToHtml } from "./md-to-html";
 import { resolve } from "path";
 import { globSync } from "glob";
+
+const addBasePathToHref = (): Plugin => {
+  let basePath: string;
+  return {
+    name: "vite-plugin-add-base-path-to-href",
+    configResolved(config) {
+      basePath = config.base;
+    },
+    transformIndexHtml(html) {
+      // <a href="/
+      // のように`/`から始まるaタグを取得するための正規表現
+      // 後読みアサーション: (?<=...), (?<!...) - JavaScript | MDN
+      // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Regular_expressions/Lookbehind_assertion
+      const hrefRE = /(?<=<a(?:\s[^>]*)*\shref\s*=\s*["'])\//gi;
+      return html.replace(hrefRE, basePath);
+    },
+  };
+};
 
 export default defineConfig({
   base: process.env.BASE_PATH || "/",
@@ -15,6 +33,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    addBasePathToHref(),
     (async () => {
       const markdown = readFileSync("./content.md");
       const { article, toc } = await markdownToHtml(markdown);
